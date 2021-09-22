9600cb2e2fded6dbee2ad3adc13bb3a3c71e998d
clairebookworm <clara32356@gmail.com>
2021-09-22 19:53:13 -0400

    page

---
+++ pages/join.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Join() {
+  return (
+    <div className={styles.container}>
+     <p className={styles.message}>Find the <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className={styles.csclub}>CS Club</a> at the Club Fair. <br />
+	Be there or be square.
+	<br />
+	We're watching...
+	</p>
+    </div>
+  )
+}
